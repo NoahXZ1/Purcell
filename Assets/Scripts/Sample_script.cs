4852630d2abb86b1b006289e25016eb7551e04e0
Xunzhuo(Noah) Zhang <xunzzhang@gmail.com>
2025-09-30 18:29:29 -0400

    Reinitialize Unity project

---
+++ Assets/Scripts/Sample_script.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sample_script : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //This is just for the test
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
